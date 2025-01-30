b9ab8afd7856cf1e1e69a1a69dd89ac1f22e877d
LiviaRLA <livialima_00@hotmail.com>
2025-01-30 10:46:20 -0300

    add Candidatar

--- src/main/java/br/com/livia/front_gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/livia/front_gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -1,6 +1,8 @@
 package br.com.livia.front_gestao_vagas.modules.candidate.controllers;
 
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -12,15 +14,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import br.com.livia.front_gestao_vagas.modules.candidate.services.ApplyJobService;
 import br.com.livia.front_gestao_vagas.modules.candidate.services.CandidateService;
 import br.com.livia.front_gestao_vagas.modules.candidate.services.FindJobsService;
 import br.com.livia.front_gestao_vagas.modules.candidate.services.ProfileCandidateService;
 import jakarta.servlet.http.HttpSession;
 
 import org.springframework.web.bind.annotation.PostMapping;
+
+
 
 
 
@@ -36,6 +42,9 @@ public class CandidateController {
 
     @Autowired
     private FindJobsService findJobsService;
+
+    @Autowired
+    private ApplyJobService applyJobService;
 
 
     @GetMapping("/login")
@@ -109,6 +118,17 @@ public class CandidateController {
 
         return "candidate/jobs";
     }
+
+    @PostMapping("/jobs/apply")
+    @PreAuthorize("hasRole('CANDIDATE')")
+    public String applyJob(@RequestParam("jobId") UUID jobId) {
+        
+        this.applyJobService.execute(getToken(), jobId);
+        System.out.println("JobId: " + jobId);
+        return "redirect:/candidate/jobs";
+    }
+    
+
 
     private String getToken() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
